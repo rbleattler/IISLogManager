71a4377653063bfcc0c19a61d39116dc509ebc6c
Robert Bleattler <Robert.Bleattler@ubc.com>
2022-02-17 12:48:05 -0500

    Cleaned up Some old code. Made WriteToFile use WriteAllBytes (performance not yet tested)

--- src/IISLogManager.Core/IISLogObjectCollection.cs
+++ src/IISLogManager.Core/IISLogObjectCollection.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Diagnostics;
 using System.Text;
 
 namespace IISLogManager.Core {
@@ -24,22 +23,9 @@ namespace IISLogManager.Core {
 		}
 
 		//TODO: https://docs.microsoft.com/en-us/dotnet/api/system.collections.concurrent.concurrentbag-1?view=net-6.0
-
-		public void OutFile(string filePath) {
-			Stopwatch stopwatch = Stopwatch.StartNew();
-			var jsonLogs = ToStringCollection();
-			StreamWriter streamWriter = new(File.Open(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite));
-			streamWriter.AutoFlush = true;
-			var processedCount = 0;
-			jsonLogs.ForEach(l => {
-				streamWriter.WriteLine(l);
-				if ( processedCount < jsonLogs.Count - 1 ) streamWriter.Write(',');
-				processedCount++;
-			});
-			stopwatch.Stop();
-			streamWriter.Close();
-			streamWriter.Dispose();
-			Console.WriteLine($"processed {jsonLogs.Count} logs in {stopwatch.Elapsed}");
+		public void WriteToFile(string filePath) {
+			var byteLogs = ToJsonByteArray();
+			File.WriteAllBytes(filePath, byteLogs);
 		}
 
 		public List<string> ToStringCollection() {
@@ -52,24 +38,9 @@ namespace IISLogManager.Core {
 			return list;
 		}
 
-		/// <summary>
-		/// This is really slow... 
-		/// </summary>
-		/// <param name="streamWriter"></param>
-		/// <param name="content"></param>
-		/// <param name="iterator"></param>
-		/// <param name="originalCount"></param>
-		/// <param name="list"></param>
-		// private void verboseWriteLine(ref StreamWriter streamWriter, string content, ref int iterator,
-		// 	int originalCount, ref List<string> list) {
-		// 	Console.Write($"Processing {iterator} of {originalCount}...\r");
-		// 	streamWriter.WriteLine(content);
-		// 	streamWriter.Flush();
-		// 	list.Remove(content);
-		// 	iterator++;
-		// }
 
-		// I know there is a better way to do this... I just don't have the bandwidth at the moment... 
+		//TODO: Fix ToJson()
+		//I know there is a better way to do this... I just don't have the bandwidth at the moment... 
 		public string ToJson() {
 			if ( !this.Any() ) return null;
 			StringBuilder stringBuilder = new();
