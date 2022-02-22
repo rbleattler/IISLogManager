3093c575015afc270c1e10643db12efdcfd15fb9
Robert Bleattler <Robert.Bleattler@ubc.com>
2022-02-21 19:11:21 -0500

    Added AuthMode and AuthToken for Remote Connections, and associated variables in other calls.

--- src/IISLogManager.CLI/CommandConfiguration.cs
+++ src/IISLogManager.CLI/CommandConfiguration.cs
@@ -4,13 +4,15 @@ using Spectre.Console;
 namespace IISLogManager.CLI;
 
 public class CommandConfiguration {
-	public IISController? IISController { get; set; }
-	public SiteObjectCollection? TargetSites { get; set; }
+	public IISController? IISController { get;  }
+	public SiteObjectCollection? TargetSites { get;  }
 	public static RunMode? RunMode { get; set; }
-	public static OutputMode? OutputMode { get; set; }
-	public string? OutputDirectory { get; set; }
-	public string? OutputUri { get; set; }
-	public Settings? Settings { get; set; }
+	public OutputMode? OutputMode { get;  }
+	public AuthMode? AuthMode { get;  }
+	public string? AuthToken { get;  }
+	public string? OutputDirectory { get;  }
+	public string? OutputUri { get;  }
+	public Settings? Settings { get;  }
 
 	public static AnsiConsoleOutput? NerdStats { get; set; }
 
@@ -21,6 +23,8 @@ public class CommandConfiguration {
 		TargetSites = new();
 		RunMode = settings.RunMode;
 		OutputMode = settings.OutputMode;
+		AuthMode = settings.AuthMode;
+		AuthToken = settings.AuthToken;
 		OutputDirectory = settings.OutputDirectory;
 		OutputUri = settings.Uri;
 		Settings = settings;
@@ -31,6 +35,8 @@ public class CommandConfiguration {
 		SiteObjectCollection? targetSites,
 		RunMode? runMode,
 		OutputMode? outputMode,
+		AuthMode? authMode,
+		string? authToken,
 		string? outputDirectory,
 		string? outputUri,
 		Settings? settings
@@ -39,6 +45,8 @@ public class CommandConfiguration {
 		TargetSites = targetSites;
 		RunMode = runMode;
 		OutputMode = outputMode;
+		AuthMode = authMode;
+		AuthToken = authToken;
 		OutputDirectory = outputDirectory;
 		OutputUri = outputUri;
 		Settings = settings;
