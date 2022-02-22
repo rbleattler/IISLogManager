e2dfc7cdde17f63561202d4b7d760b58d10b56da
Robert Bleattler <Robert.Bleattler@ubc.com>
2022-02-21 19:07:13 -0500

    Added Specialized Exception

---
+++ src/IISLogManager.Core/UriNotSpecifiedException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+
+namespace IISLogManager.Core;
+
+/// <inheritdoc />
+public class UriNotSpecifiedException : Exception {
+	public new string Message = "The uri was not specified";
+	public UriNotSpecifiedException() { }
+}
