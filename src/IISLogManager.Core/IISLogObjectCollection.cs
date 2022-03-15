0b3fa567b7cd99c6f66348676c8fccaf02d2174d
Robert Bleattler <rbleattler@outlook.com>
2022-03-15 10:46:08 -0400

    Update to support new IISLogObject paradigm. Cleanup / Syntax updates. Added explicit Json Serialization support. WriteToFile method updates to use JsonSerializer to write to file (much more efficient). Made Disposable. Added ToIISLogData method. Added UpdateLogData method. Updated ToJsonByteArray method to update log data, clear collection, clear json, and manually invoke GC.Collect.

--- src/IISLogManager.Core/IISLogObjectCollection.cs
+++ src/IISLogManager.Core/IISLogObjectCollection.cs
@@ -1,83 +1,103 @@
 #nullable enable
+using System.Collections.Concurrent;
 using System.Text;
+using Newtonsoft.Json;
 
-namespace IISLogManager.Core {
-	public class IISLogObjectCollection : List<IISLogObject>, IComparable<IISLogObjectCollection>, IComparable {
-		public IISLogObjectCollection() { }
-		public IISLogObjectCollection(IEnumerable<IISLogObject> collection) : base(collection) { }
-		public IISLogObjectCollection(int capacity) : base(capacity) { }
+namespace IISLogManager.Core;
 
-		public int CompareTo(IISLogObjectCollection other) {
-			throw new NotImplementedException();
+[Serializable]
+public class IISLogObjectCollection : List<IISLogObject>, IComparable<IISLogObjectCollection>, IComparable,
+	IDisposable {
+	public IISLogObjectCollection() { }
+	public IISLogObjectCollection(IEnumerable<IISLogObject> collection) : base(collection) { }
+	public IISLogObjectCollection(int capacity) : base(capacity) { }
+
+	private JsonSerializerSettings _jsonSerializerSettings = new JsonSerializerSettings {
+		DateTimeZoneHandling = DateTimeZoneHandling.Local,
+		DateFormatString = "yyyy'-'MM'-'dd'T'HH':'mm':'ss tt",
+		PreserveReferencesHandling = PreserveReferencesHandling.All,
+		NullValueHandling = NullValueHandling.Include,
+		MaxDepth = 3,
+		ReferenceLoopHandling = ReferenceLoopHandling.Serialize
+	};
+
+	public int CompareTo(IISLogObjectCollection other) {
+		throw new NotImplementedException();
+	}
+
+	public int CompareTo(object? obj) {
+		if ( ReferenceEquals(null, obj) ) return 1;
+		if ( ReferenceEquals(this, obj) ) return 0;
+		return obj is IISLogObjectCollection other
+			? CompareTo(other)
+			: throw new ArgumentException($"Object must be of type {nameof(IISLogObjectCollection)}");
+	}
+
+	//TODO: https://docs.microsoft.com/en-us/dotnet/api/system.collections.concurrent.concurrentbag-1?view=net-6.0
+	public void WriteToFile(string filePath, string? siteUrl = null, string? siteName = null,
+		string? hostName = null) {
+		UpdateLogData(siteUrl, siteName, hostName);
+		var targetDirectory = Path.GetDirectoryName(filePath);
+		if ( !Directory.Exists(targetDirectory) ) Directory.CreateDirectory(targetDirectory!);
+		using (StreamWriter file = File.AppendText(filePath)) {
+			Console.WriteLine("[DEBUG] Writing to file...");
+			var jsonSerializer = JsonSerializer.Create(_jsonSerializerSettings);
+			jsonSerializer.Serialize(file, this);
 		}
+	}
 
-		public int CompareTo(object? obj) {
-			if ( ReferenceEquals(null, obj) ) return 1;
-			if ( ReferenceEquals(this, obj) ) return 0;
-			return obj is IISLogObjectCollection other
-				? CompareTo(other)
-				: throw new ArgumentException($"Object must be of type {nameof(IISLogObjectCollection)}");
-		}
+	//TODO: Fix ToJson()
+	//I know there is a better way to do this... I just don't have the bandwidth at the moment... 
 
-		//TODO: https://docs.microsoft.com/en-us/dotnet/api/system.collections.concurrent.concurrentbag-1?view=net-6.0
-		public void WriteToFile(string filePath) {
-			var byteLogs = ToJsonByteArray();
-			var targetDirectory = Path.GetDirectoryName(filePath);
-			if ( !Directory.Exists(targetDirectory) ) Directory.CreateDirectory(targetDirectory!);
-			if ( File.Exists(filePath) ) File.Create(filePath);
-			File.WriteAllBytes(filePath,
-				byteLogs ?? throw new NullReferenceException("WriteToFile : byteLogs was null"));
-		}
+	public string? ToJson(string? siteUrl = null, string? siteName = null, string? hostName = null) {
+		UpdateLogData(siteUrl, siteName, hostName);
+		if ( !this.Any() ) return null;
+		TrimExcess();
+		return ToIISLogData(siteUrl, siteName, hostName).ToJson();
+		//TODO: Create an object builder for sending remote data
+		//TODO: TEST
+	}
 
-		//TODO: Fix ToJson()
-		//I know there is a better way to do this... I just don't have the bandwidth at the moment... 
+	public void FilterLogs(DateTime startDate, DateTime endDate) {
+		var filteredLogs = this.Where(l => l.DateTime >= startDate && l.DateTime <= endDate);
+		RemoveAll(l => !filteredLogs.Contains(l));
+		TrimExcess();
+	}
 
-		public string? ToJson(string? siteUrl = null, string? siteName = null, string? hostName = null) {
-			if ( !this.Any() ) return null;
-			//TODO: Create an object builder for sending remote data
-			StringBuilder stringBuilder = new();
-			stringBuilder.AppendLine("{");
-			if ( null != siteName ) {
-				stringBuilder.AppendLine($"\"SiteName\" : \"{siteName}\",");
-			}
+	private void UpdateLogData(string? siteUrl = null, string? siteName = null,
+		string? hostName = null) {
+		Parallel.ForEach(this, log => {
+			log.SiteName = siteName;
+			log.SiteUrl = siteUrl;
+			log.HostName = hostName;
+		});
+	}
 
-			if ( null != siteUrl ) {
-				stringBuilder.AppendLine($"\"SiteUrl\" : \"{siteUrl}\",");
-			}
+	public IISLogData ToIISLogData(string? siteUrl = null, string? siteName = null,
+		string? hostName = null) {
+		TrimExcess();
+		UpdateLogData(siteUrl, siteName, hostName);
+		var iisLogObjectCollection = this;
+		IISLogData toOut = new IISLogData(siteUrl, siteName, hostName, ref iisLogObjectCollection);
+		TrimExcess();
+		GC.Collect();
+		return toOut;
+	}
 
-			if ( null != hostName ) {
-				stringBuilder.AppendLine($"\"HostName\" : \"{hostName}\",");
-			}
+	public byte[] ToJsonByteArray(string? siteUrl = null, string? siteName = null, string? hostName = null) {
+		UpdateLogData(siteUrl, siteName, hostName);
+		var jsonOut = ToJson(siteUrl ?? null, siteName ?? null, hostName ?? null);
+		Clear();
+		var bytesOut = Encoding.ASCII.GetBytes(jsonOut);
+		jsonOut = null;
+		GC.Collect();
+		return bytesOut;
+		// return null;
+	}
 
-			stringBuilder.AppendLine("\"Logs\":[");
-			var processedCount = 0;
-			foreach (var site in this) {
-				var json = site.ToJson();
-				if ( processedCount < Count - 1 ) {
-					stringBuilder.AppendFormat("{0},", json)
-						.AppendLine();
-				} else {
-					stringBuilder.AppendFormat("{0}", json)
-						.AppendLine();
-				}
-
-				processedCount++;
-			}
-
-			stringBuilder.AppendLine("]");
-			stringBuilder.AppendLine("}");
-			return stringBuilder.ToString();
-		}
-
-		public void FilterLogs(DateTime startDate, DateTime endDate) {
-			var filteredLogs = this.Where(l => l.LogDateTime >= startDate && l.LogDateTime <= endDate);
-			RemoveAll(l => !filteredLogs.Contains(l));
-		}
-
-		public byte[]? ToJsonByteArray(string? siteUrl = null, string? siteName = null, string? hostName = null) {
-			var jsonOut = ToJson(siteUrl ?? null, siteName ?? null, hostName ?? null);
-			if ( null != jsonOut ) return Encoding.ASCII.GetBytes(jsonOut);
-			return null;
-		}
+	public void Dispose() {
+		Clear();
+		GC.Collect();
+		GC.SuppressFinalize(this);
 	}
 }
