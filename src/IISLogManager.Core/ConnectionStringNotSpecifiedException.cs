1bb0b3320913ff336ec35aca68f63df8117b1623
Robert Bleattler <rbleattler@outlook.com>
2022-03-28 16:27:20 -0400

    Added ConfigurationFileType enum, and ConnectionStringNotSpecifiedException exception

---
+++ src/IISLogManager.Core/ConnectionStringNotSpecifiedException.cs
@@ -0,0 +1,7 @@
+﻿namespace IISLogManager.Core;
+
+/// <inheritdoc />
+public class ConnectionStringNotSpecifiedException : Exception {
+	/// <inheritdoc />
+	public override string Message => "The connection string was not specified";
+}
