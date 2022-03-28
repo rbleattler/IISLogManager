bd1015d6ce8e3a704c23eb078acb782cceea1486
Robert Bleattler <rbleattler@outlook.com>
2022-03-28 16:26:56 -0400

    Moved processing logic to methods. Added scaffolding for future optimization.

--- src/IISLogManager.CLI/CommandProcessor.cs
+++ src/IISLogManager.CLI/CommandProcessor.cs
@@ -69,146 +69,23 @@ public class CommandProcessor {
 	public void ProcessLogs(ref CommandConfiguration config) {
 		//TODO: Add verbose output
 		if ( config.TargetSites != null ) {
-			// AnsiConsole.MarkupLine("[[DEBUG]] TargetSites is not null...");
-			if ( config.Settings is {Filter: true} ) {
-				int pathCount = 0;
-#if DEBUG
-				config.TargetSites.ForEach(p => pathCount += p.LogFilePaths.Count);
-				AnsiConsole.MarkupLine($"[[DEBUG]] Filtering enabled...");
-				AnsiConsole.MarkupLine($"[[DEBUG]] FromDate : {config.Settings.FromDate} ...");
-				AnsiConsole.MarkupLine($"[[DEBUG]] ToDate : {config.Settings.ToDate} ...");
-				AnsiConsole.MarkupLine($"[[DEBUG]] Filtering Files... ( Count : {pathCount})");
-				pathCount = 0;
-#endif
-				config.TargetSites.FilterAllLogFiles(
-					DateTime.Parse(config.Settings.FromDate ??
-					               DateTime.Today.AddMonths(-1).ToString(CultureInfo.CurrentCulture)
-					),
-					DateTime.Parse(config.Settings.ToDate ??
-					               DateTime.Today.ToString(CultureInfo.CurrentCulture)
-					)
-				);
-				config.TargetSites.ForEach(p => pathCount += p.LogFilePaths.Count);
-				AnsiConsole.MarkupLine(
-					$"[[DEBUG]] Finished Filtering Files... (New Count : {pathCount})");
-			}
+			FilterLogs(ref config);
+			if ( config.TargetSites != null )
+				foreach (var site in config.TargetSites) {
+					var targetSite = site;
+					AnsiConsole.MarkupLine($"[[DEBUG]] Processing {site.SiteName}...");
+					if ( config.OutputMode != OutputMode.LocalDb ) {
+						site.ParseAllLogs(); //TODO: Update to implement as a dependent process of the output mode and size/number of logs for a given site
+						if ( site.Logs.Count <= 0 ) { }
 
-			foreach (var site in config.TargetSites) {
-				AnsiConsole.MarkupLine($"[[DEBUG]] Processing {site.SiteName}...");
-				if ( config.OutputMode != OutputMode.LocalDb ) {
-					site.ParseAllLogs(); //TODO: Update to implement as a dependent process of the output mode and size/number of logs for a given site
-					if ( site.Logs.Count <= 0 ) { }
-
-					AnsiConsole.MarkupLine($"[DarkOrange]{site.SiteName}[/] has no logs from the target date range...");
-					continue;
-				}
-
-				switch (config.OutputMode) {
-					case OutputMode.Local: {
-						AnsiConsole.MarkupLine($"[[DEBUG]] Output mode Local...");
-						var outFile = site.GetLogFileName(config.OutputDirectory);
-						site.Logs.TrimExcess();
-						site.Logs.WriteToFile(outFile);
-						site.Logs.Clear();
-						site.Logs.Dispose();
-						AnsiConsole.MarkupLine($"[DarkOrange]Output File :[/] {outFile}");
-						break;
+						AnsiConsole.MarkupLine(
+							$"[DarkOrange]{site.SiteName}[/] has no logs from the target date range...");
+						continue;
 					}
-					case OutputMode.Remote: {
-						AnsiConsole.MarkupLine($"[[DEBUG]] Output mode Remote...");
-						if ( string.IsNullOrWhiteSpace(config.OutputUri) ) {
-							throw new UriNotSpecifiedException();
-						}
 
-						if ( config.OutputUri != null ) {
-							ConnectionManager connectionManager = new();
-							connectionManager.SetConnection(config.OutputUri);
-							if ( config.AuthMode == AuthMode.BearerToken ) {
-								if ( config.AuthToken != null ) connectionManager.BearerToken = config.AuthToken;
-								connectionManager.SetConnection(
-									config.OutputUri,
-									connectionManager.BearerToken ?? throw new NullAuthTokenException()
-								);
-							}
-
-							var response =
-								connectionManager.AddLogs(site.Logs, site.SiteUrl, site.SiteName, site.HostName);
-							// var response = connectionManager.AddLogs(site.Logs, site.SiteUrl, site.SiteName, site.HostName,
-							// 	true);
-							AnsiConsole.MarkupLine($"[DarkOrange]Server Response :[/]{response}");
-						}
-
-						if ( config.OutputUri == null ) {
-							throw new WarningException("OutputUri was not specified.");
-						}
-
-						break;
-					}
-					case OutputMode.LocalDb:
-						var iisLogManagerContext = new IISLogManagerContext(
-							config.DatabaseProvider ?? DatabaseProvider.Sqlite,
-							config.ConnectionString,
-							config.Settings?.TableName ?? "IISLogs"
-						);
-						iisLogManagerContext.Database.EnsureCreated();
-						var fileCount = site.LogFilePaths.Count();
-						var processed = 0;
-						float fileSizeProcessed = 0;
-						List<FileInfo> AllFiles = new();
-						site.LogFilePaths.ForEach(p => AllFiles.Add(new FileInfo(p)));
-						float totalFileSize = 0;
-						AllFiles.ForEach(f => totalFileSize += f.Length / 1024f / 1024f);
-						var progress = AnsiConsole.Progress()
-							.AutoRefresh(true)
-							.AutoClear(false)
-							.HideCompleted(true)
-							.Columns(new ProgressColumn[] {
-								new SpinnerColumn(Spinner.Known.Aesthetic),
-								new PercentageColumn(),
-								new TaskDescriptionColumn(),
-								new ProgressBarColumn(),
-								new ElapsedTimeColumn()
-							});
-						// progress.RefreshRate = TimeSpan.FromSeconds(1);
-						progress.Start(
-							ctx => {
-								var ParseLogsTask = ctx.AddTask("[green]Parsing Logs[/]")
-									.MaxValue(fileCount);
-								ParseLogsTask.StartTask();
-								site.LogFilePaths.ForEach(p => {
-									var fileInfo = (new FileInfo(p));
-									var fileSizeMegs = (fileInfo.Length / 1024f / 1024f);
-									ParseLogsTask.Description =
-										$"[DarkOrange]{Path.GetFileName(p)}[/] | [DarkOrange]File Size[/] : {fileSizeMegs.ToString("F")} MB / {totalFileSize.ToString("F")} MB | [DarkOrange]File:[/] {processed}/{fileCount} ";
-									site.ParseLogs(p);
-									if ( site.Logs != null ) {
-										iisLogManagerContext.AddRange(site.Logs);
-										iisLogManagerContext.SaveChanges();
-										site.Logs.Clear();
-									}
-
-									GC.Collect();
-									processed++;
-									fileSizeProcessed += fileSizeMegs;
-									ParseLogsTask.Increment(1);
-									ctx.Refresh();
-								});
-
-								ParseLogsTask.StopTask();
-							}
-						);
-
-						break;
-					case OutputMode.RemoteDb:
-						break;
-					case null:
-						break;
-					default:
-						throw new ArgumentOutOfRangeException();
+					ProcessSite(config, ref targetSite);
+					site.Dispose();
 				}
-
-				site.Dispose();
-			}
 		}
 	}
 
@@ -296,7 +173,8 @@ public class CommandProcessor {
 						var iisLogManagerContext = new IISLogManagerContext(
 							config.DatabaseProvider ?? DatabaseProvider.Sqlite,
 							config.ConnectionString,
-							config.Settings?.TableName ?? "IISLogs"
+							config.Settings?.TableName ?? "IISLogs",
+							config.Settings?.ExcludeFields.Split(',')
 						);
 						iisLogManagerContext.Database.EnsureCreated();
 						var fileCount = site.LogFilePaths.Count();
@@ -333,7 +211,8 @@ public class CommandProcessor {
 										var innerContext = new IISLogManagerContext(
 											refConfig?.DatabaseProvider ?? DatabaseProvider.Sqlite,
 											refConfig?.ConnectionString,
-											refConfig.Settings?.TableName ?? "IISLogs"
+											refConfig.Settings?.TableName ?? "IISLogs",
+											refConfig.Settings?.ExcludeFields.Split(',')
 										);
 										await innerContext.Database.EnsureCreatedAsync();
 										var fileInfo = (new FileInfo(p));
@@ -411,4 +290,167 @@ public class CommandProcessor {
 	}
 
 	public static CommandProcessor Instance = new();
+
+	private void FilterLogs(ref CommandConfiguration config) {
+		if ( config.Settings is {Filter: true} ) {
+			int pathCount = 0;
+#if DEBUG
+				config.TargetSites.ForEach(p => pathCount += p.LogFilePaths.Count);
+				AnsiConsole.MarkupLine($"[[DEBUG]] Filtering enabled...");
+				AnsiConsole.MarkupLine($"[[DEBUG]] FromDate : {config.Settings.FromDate} ...");
+				AnsiConsole.MarkupLine($"[[DEBUG]] ToDate : {config.Settings.ToDate} ...");
+				AnsiConsole.MarkupLine($"[[DEBUG]] Filtering Files... ( Count : {pathCount})");
+				pathCount = 0;
+#endif
+			config.TargetSites.FilterAllLogFiles(
+				DateTime.Parse(config.Settings.FromDate ??
+				               DateTime.Today.AddMonths(-1).ToString(CultureInfo.CurrentCulture)
+				),
+				DateTime.Parse(config.Settings.ToDate ??
+				               DateTime.Today.ToString(CultureInfo.CurrentCulture)
+				)
+			);
+			config.TargetSites.ForEach(p => pathCount += p.LogFilePaths.Count);
+			AnsiConsole.MarkupLine(
+				$"[[DEBUG]] Finished Filtering Files... (New Count : {pathCount})");
+		}
+	}
+
+	private void ProcessSite(CommandConfiguration config, ref SiteObject siteReference) {
+		var site = siteReference;
+		switch (config.OutputMode) {
+			case OutputMode.Local: {
+				AnsiConsole.MarkupLine($"[[DEBUG]] Output mode Local...");
+				var outFile = site.GetLogFileName(config.OutputDirectory);
+				site.Logs.TrimExcess();
+				site.Logs.WriteToFile(outFile);
+				site.Logs.Clear();
+				site.Logs.Dispose();
+				AnsiConsole.MarkupLine($"[DarkOrange]Output File :[/] {outFile}");
+				break;
+			}
+			case OutputMode.Remote: {
+				AnsiConsole.MarkupLine($"[[DEBUG]] Output mode Remote...");
+				if ( string.IsNullOrWhiteSpace(config.OutputUri) ) {
+					throw new UriNotSpecifiedException();
+				}
+
+				if ( config.OutputUri != null ) {
+					ConnectionManager connectionManager = new();
+					connectionManager.SetConnection(config.OutputUri);
+					if ( config.AuthMode == AuthMode.BearerToken ) {
+						if ( config.AuthToken != null ) connectionManager.BearerToken = config.AuthToken;
+						connectionManager.SetConnection(
+							config.OutputUri,
+							connectionManager.BearerToken ?? throw new NullAuthTokenException()
+						);
+					}
+
+					var response =
+						connectionManager.AddLogs(site.Logs, site.SiteUrl, site.SiteName, site.HostName);
+					AnsiConsole.MarkupLine($"[DarkOrange]Server Response :[/]{response}");
+				}
+
+				if ( config.OutputUri == null ) {
+					throw new WarningException("OutputUri was not specified.");
+				}
+
+				break;
+			}
+			case OutputMode.LocalDb:
+				ProcessLocalDbSite(config, ref site);
+				break;
+			case null:
+				break;
+			default:
+				throw new ArgumentOutOfRangeException();
+		}
+
+		site.Dispose();
+	}
+
+	private void ProcessLocalDbSite(CommandConfiguration config, ref SiteObject siteReference) {
+		var site = siteReference;
+		var iisLogManagerContext = new IISLogManagerContext(
+			config.DatabaseProvider ?? DatabaseProvider.Sqlite,
+			config.ConnectionString ?? throw new ConnectionStringNotSpecifiedException(),
+			config.Settings?.TableName ?? "IISLogs",
+			config.Settings?.ExcludeFields?.Split(',')
+		);
+		iisLogManagerContext.Database.EnsureCreated();
+		var fileCount = site.LogFilePaths.Count();
+		var processed = 0;
+		float fileSizeProcessed = 0;
+		List<FileInfo> allFiles = new();
+		site.LogFilePaths.ForEach(p => allFiles.Add(new FileInfo(p)));
+		float totalFileSize = 0;
+		allFiles.ForEach(f => totalFileSize += f.Length / 1024f / 1024f);
+		var progress = AnsiConsole.Progress()
+			.AutoRefresh(true)
+			.AutoClear(false)
+			.HideCompleted(true)
+			.Columns(new ProgressColumn[] {
+				new SpinnerColumn(Spinner.Known.Aesthetic),
+				new PercentageColumn(),
+				new TaskDescriptionColumn(),
+				new ProgressBarColumn(),
+				new ElapsedTimeColumn()
+			});
+		// progress.RefreshRate = TimeSpan.FromSeconds(1);
+		progress.Start(
+			ctx => {
+				var parseLogsTask = ctx.AddTask("[green]Parsing Logs[/]")
+					.MaxValue(fileCount);
+				parseLogsTask.StartTask();
+				site.LogFilePaths.ForEach(p => {
+					var totalLines = (int) Utils.CountLines(p);
+					var headerLine = Utils.GetHeaderLine(p); //TODO: Add headerLine to Parse calls
+					var maxProcessCount = 250000;
+					//TODO: We should add functionality to determine what an appropriate number of logs per available RAM is
+					var fileInfo = (new FileInfo(p));
+					var fileSizeMegs = (fileInfo.Length / 1024f / 1024f);
+					if ( totalLines > maxProcessCount ) {
+						int processedCount = 0;
+						parseLogsTask.Description =
+							$"[DarkOrange]{Path.GetFileName(p)}[/] ({processedCount}/{totalLines}) | [DarkOrange]File Size[/] : {fileSizeMegs.ToString("F")} MB / {totalFileSize.ToString("F")} MB | [DarkOrange]File:[/] {processed}/{fileCount} ";
+						while (processedCount < totalLines) {
+							int remaining = totalLines - processedCount;
+							var processMax = remaining >= maxProcessCount;
+							var startLine = processedCount;
+							int processCount = processMax ? maxProcessCount : remaining;
+							site.ParseLogs(p, startLine, processCount, headerLine);
+							if ( site.Logs != null ) {
+								iisLogManagerContext.AddRange(site.Logs);
+								iisLogManagerContext.SaveChanges();
+								site.Logs.Clear();
+								GC.Collect();
+								fileSizeProcessed += fileSizeMegs / maxProcessCount;
+								ctx.Refresh();
+							}
+						}
+					} else {
+						parseLogsTask.Description =
+							$"[DarkOrange]{Path.GetFileName(p)}[/] | [DarkOrange]File Size[/] : {fileSizeMegs.ToString("F")} MB / {totalFileSize.ToString("F")} MB | [DarkOrange]File:[/] {processed}/{fileCount} ";
+						site.ParseLogs(p);
+						if ( site.Logs != null ) {
+							iisLogManagerContext.AddRange(site.Logs);
+							iisLogManagerContext.SaveChanges();
+							site.Logs.Clear();
+						}
+
+						fileSizeProcessed += fileSizeMegs;
+					}
+
+					GC.Collect();
+					processed++;
+					parseLogsTask.Increment(1);
+
+
+					ctx.Refresh();
+				});
+
+				parseLogsTask.StopTask();
+			}
+		);
+	}
 }
